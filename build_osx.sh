28e0993ead254c6c5c7a7c0634ac2e36d4024943
Kyle Lacy <kylewlacy@me.com>
2012-06-09 22:18:37 -0700

    Initial commit

---
+++ build_osx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cflags="-Wno-import"
+ldflags="-lobjc -framework Foundation -framework Cocoa"
+
+for file in platforms/osx/src/*.mm; do
+  files="$files$file "
+done
+for file in src/*.cpp; do
+  files="$files$file "
+done
+files="$files platforms/osx/src/main.m"
+
+appdir="build/Zombiez.app"
+mkdir -p $appdir/Contents/{_CodeSignature,Frameworks,MacOS,Plugins,Resources,Versions}
+
+for file in $files; do
+  if [ -e "$file" ]; then
+    fileobj=$(basename "$file")
+    fileobj=${fileobj%.*}
+    fileobj="build/$fileobj.o"
+    gcc $cflags -c -o "$fileobj" "$file"
+
+    fileobjs="$fileobjs$fileobj "
+  fi
+done
+gcc $cflags $ldflags -o $appdir/Contents/MacOS/zombiez2013 $fileobjs
+touch $appdir/Contents/PkgInfo
+cp platforms/osx/Info.plist $appdir/Contents/Info.plist
+cp platforms/osx/resources/* $appdir/Contents/Resources/
+rm build/*.o
